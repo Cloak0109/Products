ecbc26dd20a13a952ae024a2618f1975fdb38ccc
Shubham Singh <Shubham.kr.singh0109@gmail.com>
2024-06-20 10:46:10 +0530

    done

--- index.js
+++ index.js
@@ -1,48 +1,23 @@
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
-
 const dburl="mongodb+srv://shubhamkrsingh0109:4vjsvZRcqKd5Udcy@cloak0109.ydnucal.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cloak0109";
+const productRoutes = require("./routes/productRoutes");
 
-const products=[
-    {id: 1, name: "apple"},
-    {id: 2, name: "banana"},
-    {id: 3, name: "charizard"},
-]
+app.use(express.json());//middleware
 
-const productSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: String,
-      required: true,
-    },
-  
-    brand: {
-      type: String,
-      required: true,
-    },
-  
-    category: {
-      type: String,
-      required: true,
-    },
-  }, { timestamps: true });
-  
-const productModel = mongoose.model('products', productSchema);
-
+// middleware takes a req then change its format as per you requirement.
+// const products=[
+//     {id: 1, name: "apple"},
+//     {id: 2, name: "banana"},
+//     {id: 3, name: "charizard"},
+// ]
 
 mongoose.connect(dburl).then((function() {
     console.log("connected to db");
 })).catch(err => console.log(err));
 
-app.get('/api/products', async (req, res) =>{
-    let products = await productModel.find();
-    res.json(products);
-})
+app.use("/api/products", productRoutes);
 
 //create server
 app.listen(3000,() =>{
